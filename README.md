a90444f101bd7bb5d1d83d703b7e8af317b046a9
Root0110 <xw41@illinois.edu>
2019-12-02 10:37:40 -0600

    Define functions

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ By Xiaohan Wang
 2019 Fall
 
 ## Project Background
+Wealth inequality in the United States is high and has increased sharply in recent decades.
 The Monte Carlo simulation involves running many scenarios with different random inputs and summarizing the distribution of the results from potential result. This distribution can inform the likelihood that the result will be within a certain window. 
 
 Many researchers are devoted to design a set of rules to maintain relative justice in the society, which matters a lot to all of us. Before that, we need to understand the distribution of the wealth within the whole society, to see how it would be assigned to each individual. And since the amount of money one person earns and donates are two random variables, so I would apply Monte Carlo Simulation method to dive deeply into the distribution of money one person owns. Furthermore, I would love to test whether some strategies about dealing with the social wealth id reasonable or not. 
@@ -26,9 +27,10 @@ It's realistic that some of the social members are born with some assets, like t
 
 2.__Personal Income__    
 Income is the main part in our assets, including stable income like salary from hard work, and unstable income like gains or loss from stock or lottery.  
+Personal income is an individual's total earnings from wages, investment interest, and other sources. 
 
 3.__Tax__   
-It's an efficient way for the governemnt to get citizens paying all kinds of tax to be involved in the construction of country. Here I focus on income tax rate. And to simplify the real situation, I made assumption that people with top 50% income should pay x% tax, and it's necessary for the rest.
+It's an efficient way for the government to get citizens paying all kinds of tax to be involved in the construction of country. Here I focus on income tax rate. And to simplify the real situation, I made assumption that people with top 50% income should pay x% tax, and it's necessary for the rest.
 
 4.__Social Welfare__   
 In modern society, the government feels responsible for providing some welfare for __every__ social member. It can be medical insurance, pension fee, and other kinds of life guarantee. This is a kind of stable addition to personal wealth, and it's not distinguishing between the poor and the rich.
--- simulation.py
+++ simulation.py
@@ -15,13 +15,13 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import math
 
-
+'''
 # set rules to change personal wealth amount
 # No.1 personal income
 # According to the Current Population Survey(CPS) conducted by US Census Bureau,
 dataset = pd.read_table('personal_income_2019')
-'''
-# 大概还原real data
+
+# explore the distribution of  real data
 n = dataset.count()
 income_points = []
 for i in range(n[0]):
@@ -33,41 +33,43 @@ sns.distplot(income_points)
 
 
 # generate a list of personal income values based on historical distribution
-def StableIncomeSample(size):
-    ''' Combine 3 distributions in piecewise fashion to generate random sample
+def stable_income_sample(size):
+    ''' Combine 3 distributions in piecewise fashion to generate random sample with income values
     :param size: number of people
-    :return: sample: a list of personal income values
+    :return: a ndarray of personal income values
     '''
-    sample_left = [0]*math.ceil(0.286 * size)
-    sample_middle = list(np.random.triangular(0, 1250, 100000, math.ceil(0.643 * size)))
-    sample_right = list(np.random.choice([100000, 10000000000], math.floor(0.071 * size)))
-    sample = sample_left + sample_middle + sample_right
-    return sample
+    sample_left = np.ndarray([0]*math.ceil(0.286 * size))
+    sample_middle = np.random.triangular(0, 1250, 100000, math.ceil(0.643 * size))
+    sample_right = np.random.choice([100000, 10000000000], math.floor(0.071 * size))
+    sample = np.append(sample_left,sample_middle,sample_right)
+    return np.random.shuffle(sample)
 
-def randomUnstableIncome(person_n,chance):
+
+def random_unstable_income(people,chance,amount):
+    ''' Given a group of people, the chance of winning a certain amount of unstable income,
+    get a column of unstable income for each person.
+    :param people: a list of ID numbers which represents a group of people
+    :param chance: the chance of earning unstable income
+    :param amount: the amount of unstable income
+    :return: unstable_df: a dataframe that contains unstable income values for each person
     '''
-    :param size:
-    :return:
-    '''
-    id_choice0 = list(np.random.choice(person_n, math.ceil(chance * len(person_n))))
+    id_choice0 = list(np.random.choice(people, math.ceil(chance * len(people))))
     # unstable income like gains from lottery
-    # assume the probability of winning $1 is 1/100
-    unstable_df = pd.DataFrame({'income_unstable': [5] * len(id_choice0)}, index=id_choice0)
+    # assume the probability of winning $5 is 1/100
+    unstable_df = pd.DataFrame({'income_unstable': [amount] * len(id_choice0)}, index=id_choice0)
     return unstable_df
 
 
-
-def simulation(person_n,fortune,round_no):
+def simulation(people,fortune_df,round_no):
     ''' Simulate the gain and loss of personal wealth in
-    :param fortune: the dataframe that contains initial personal wealth value of the whole people
+    :param people: a list of ID numbers which represents a group of people
+    :param fortune_df: the dataframe that contains initial personal wealth value of the whole people
     :param round_no: the number of simulation rounds
-    :return: fortune: a column of new fortune value after one round of simulation
+    :return: fortune_i: a column of new fortune value after one round of simulation
     '''
-    # if there're people with zero wealth,
-    # if len(fortune[fortune[round_no - 1]]==0) > 0:
-    round_i = pd.DataFrame({'pre_round_wealth':fortune[round_no - 1],'income_stable':StableIncomeSample(len(person_n)),
-                            'income_tax':0},index=person_n)
-    round_i = pd.merge(round_i, randomUnstableIncome(person_n,1/100), how='left', left_index=True, right_index=True)
+    round_i = pd.DataFrame({'pre_round_wealth':fortune_df[round_no - 1],'income_stable':stable_income_sample(len(people)),
+                            'income_tax':0})
+    round_i = pd.merge(round_i, random_unstable_income(people,1/100), how='left', left_index=True, right_index=True)
     round_i.fillna(0,inplace=True)
     # personal income tax rate
     round_i['income_tax'] = round_i['income_stable'].apply(lambda x: 0.1 * x if x <= 9700 else (
@@ -80,12 +82,12 @@ def simulation(person_n,fortune,round_no):
                         0.1 * 9700 + 0.12 * 30045 + 0.22 * 42655 + 0.24 * 78325 + 0.32 * 43375 + 0.35 * 306200 + 0.37 * (
                                     x - 510300)))))))
 
-    # social welfare: unemployment benefits, assume $400/week for at 6 weeks, isn't taxable
+    # social welfare: unemployment benefits, assume $400/week for at most 6 weeks, isn't taxable
     # The standard time-length of unemployment compensation is six months
     round_i['social_welfare'] = round_i['income_stable'].apply(lambda x: 2400 if x == 0 else 0)
 
     # accidental_loss
-    id_choice1 = list(np.random.choice(person_n, math.ceil((1 / 50) * len(person_n))))
+    id_choice1 = list(np.random.choice(people, math.ceil((1 / 50) * len(people))))
     temp1 = pd.DataFrame({'accidental_loss': [500] * len(id_choice1)}, index=id_choice1)
     round_i = pd.merge(round_i, temp1, how='left', left_index=True, right_index=True)
     round_i.fillna(0,inplace=True)
@@ -93,7 +95,7 @@ def simulation(person_n,fortune,round_no):
     # a column of fortune value after one round
     fortune_i = round_i['pre_round_wealth'] + round_i['income_stable'] + round_i['income_unstable'] - \
                 round_i['income_tax'] + round_i['social_welfare'] - round_i['accidental_loss']
-    #fortune_i.astype('Int64')
+    fortune_i.astype('Int64')
     return fortune_i
 
 
@@ -109,3 +111,32 @@ for round in range(1,100):
 result1 = fortune.T
 
 
+"""
+os.chdir('/Users/W/PycharmProjects/final_project/pic1')
+
+
+def graph1(fortune_i,start,end,length):
+    '''
+
+    :param fortune:
+    :param start:
+    :param end:
+    :param length:
+    :return:
+    '''
+    for n in list(range(start,end,length)):
+        value = fortune_i.iloc[n]
+        plt.figure(figsize=(10,6))
+        plt.bar(value.index,value.values,color='gray',alpha=0.8,width=0.9)
+        plt.ylim([0,10000000000])
+        plt.xlim([0,10001])
+        plt.title('Round {}'.format(n))
+        plt.xlabel('PlayerID')
+        plt.ylabel('Personal wealth')
+        plt.grid(color='gray',linestyle='--',linewidth=0.5)
+        plt.savefig('graph1_round_{}'.format(n),dpi=200)
+        print('success in plotting round {}'.format(n))
+
+
+graph1(result1,0,100,10)
+"""
